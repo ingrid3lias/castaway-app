e13fb0c955f38d3dea3ac268f4cd16e45c2ff1ff
Ingrid Elias <elias.ingrid@yahoo.com>
2016-03-17 11:42:11 +0900

    Fixes issue with header background image

--- app/scripts/app.podcasts/content/podcastAnnouncement.directive.js
+++ app/scripts/app.podcasts/content/podcastAnnouncement.directive.js
@@ -16,15 +16,10 @@
     function PodcastAnnouncementCtrl ($stateParams, AnnouncementList) {
       var vm = this;
       
-//      // Filter
-//      vm.announcementsFitler = function () {
-//        $stateParams.podcast_id = vm.announcements.podcast_id
-//      }
-      
       // Load Announcements
       vm.podcastsLoading = true;
       vm.podcastsLoaded = false;
-      vm.announcements = AnnouncementList.query();
+      vm.announcements = AnnouncementList.get({podcast_id: $stateParams.podcast_id});
       vm.announcements.$promise
         .then(function () {
           vm.podcastsLoaded = true;
