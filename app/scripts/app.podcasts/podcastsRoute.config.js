fdd9f11eae7772a58982a819e9e0134d1cd8498c
Ingrid Elias <elias.ingrid@yahoo.com>
2016-03-27 11:35:04 +0900

    cleans up the podcastroute

--- app/scripts/app.podcasts/podcastsRoute.config.js
+++ app/scripts/app.podcasts/podcastsRoute.config.js
@@ -59,40 +59,6 @@
           }
         }
       })
-    // Episode Index
-//      .state('podcast.episodeIndex', {
-//        url: '/episode/:episode_id',
-//        views: {
-//          'navigation': {
-//            templateUrl: 'views/podcasts/episode/episode-navigation.html',
-//          },
-//          'header': {
-//           
-//          },
-//          'sidebar': {
-//            template: '<ca-podcast-sidebar></ca-podcast-sidebar>'
-//          },
-//          'content': {
-//            templateUrl: 'views/podcasts/index.html'
-//          }
-//        }
-//      })
-//      .state('podcast.episodeIndex.index', {
-//        url: '/index',
-//        template: '<ca-podcast-index></ca-podcast-index>'
-//      })
-//      .state('podcast.episodeIndex.station', {
-//        url: '/station',
-//        template: '<ca-podcast-station-index></ca-podcast-station-index>'
-//      })
-//      .state('podcast.episodeIndex.announcement', {
-//        url: '/announcement',
-//        template: '<ca-podcast-announcement-index></ca-podcast-announcement-index>'
-//      })
-//      .state('podcast.episodeIndex.review', {
-//        url: '/review',
-//        template: '<ca-podcast-review-index></ca-podcast-review-index>'
-//      })
     //end of states
   }
 })();
